70f90973fd717086449aba7b7bca9d23d421ec19
Laszlo_Szikszai <szikszai.laszlo@wolterskluwer.com>
2017-03-09 14:52:00 +0100

    Made initial state

--- po/homePage.js
+++ po/homePage.js
@@ -4,11 +4,6 @@ function HomePage(browser) {
     this.get = function () {
         return browser.get(this.url);
     };
-
-    this.clickMenuItem = function (item) {
-        var menuItem = {css: 'a[href*="' + item + '"]'};
-        return browser.findElement(menuItem).click();
-    }
-};
+}
 
 module.exports = HomePage;
